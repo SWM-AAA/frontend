5c2aca49289a53ce14329953d38ec3193c76a50a
nakevin96 <nakevin96@hanyang.ac.kr>
2023-06-27 13:45:55 +0900

    [TAS-80]feature: sample Rootab만든 후 LoginScreen에서 kakaoLoginButton눌렀을 때 RootTab으로 라우팅, 아직 token은 안 붙어 있음

--- lib/user/screens/components/kakao_login_button.dart
+++ lib/user/screens/components/kakao_login_button.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class KakaoLoginButton extends StatelessWidget {
   final double? width;
   final double? fontSize;
+  final void Function()? onPressed;
 
   const KakaoLoginButton({
     Key? key,
     this.width = 240,
     this.fontSize = 20,
+    this.onPressed,
   }) : super(key: key);
 
   @override
@@ -15,7 +17,7 @@ class KakaoLoginButton extends StatelessWidget {
     return SizedBox(
       width: width, // 너비 설정
       child: ElevatedButton(
-        onPressed: () {}, // onPressed 빈칸으로 두기
+        onPressed: onPressed ?? () {},
         style: ElevatedButton.styleFrom(
           backgroundColor: const Color(0xFFFFE600), // 카카오 색상 설정
           padding: const EdgeInsets.symmetric(
--- lib/user/screens/login_screen.dart
+++ lib/user/screens/login_screen.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/common/components/animated_app_title.dart';
 import 'package:frontend/common/layouts/default_layout.dart';
+import 'package:frontend/common/screens/root_tab.dart';
 import 'package:frontend/user/screens/components/kakao_login_button.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const DefaultLayout(
-      backgroundDecorationImage: DecorationImage(
+    void onKakaoLoginButtonClick() {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => const RootTab(),
+        ),
+      );
+    }
+
+    return DefaultLayout(
+      backgroundDecorationImage: const DecorationImage(
         fit: BoxFit.cover,
         image: AssetImage(
           'assets/images/backgrounds/sample_background.png',
@@ -19,7 +33,7 @@ class LoginScreen extends StatelessWidget {
         top: true,
         bottom: false,
         child: Padding(
-          padding: EdgeInsets.symmetric(
+          padding: const EdgeInsets.symmetric(
             horizontal: 16.0,
           ),
           child: Column(
@@ -30,13 +44,14 @@ class LoginScreen extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    AnimatedAppName(),
-                    SizedBox(
+                    const AnimatedAppName(),
+                    const SizedBox(
                       height: 40,
                     ),
                     KakaoLoginButton(
                       width: 248,
                       fontSize: 16,
+                      onPressed: onKakaoLoginButtonClick,
                     ),
                   ],
                 ),
